43c609703e8b4c85f666b9f9552808e54658cc5e
WeepingDogel <weepingdogel@gmail.com>
2023-02-18 17:38:13 +0800

    config file.

---
+++ app/config.py
@@ -0,0 +1,8 @@
+# Authentication
+SECRET_KEY = "0ee5776b64d0b51644cb60baef21d4f76ee97ec497abbae2f20f3dd18a665049"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+# DATABASE
+DATABSE_URL = "sqlite:///./database/database.sqlite"
