0ca560691168a3f3bacd861ed2fef87408f9f7c0
WeepingDogel <weepingdogel@gmail.com>
2023-02-26 19:40:47 +0800

    CRUD

--- app/model/crud.py
+++ app/model/crud.py
@@ -1,22 +1,40 @@
+import time
+import uuid
+
 from sqlalchemy.orm import Session
-import time, uuid
+
 from . import models, schemas
 
 
-def CreateUser(db: Session, user: schemas.User):
+def create_user(db: Session, user: schemas.User):
     date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    userUUID = str(uuid.uuid4())
+    user_uuid = str(uuid.uuid4())
     db_user = models.User(
-        userName = user.userName,
-        password = user.password,
-        email = user.email,
-        date = date,
-        usersUUID = userUUID
+        user_name=user.user_name,
+        password=user.password,
+        email=user.email,
+        date=date,
+        users_uuid=user_uuid
     )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
-def GetUserByName(db: Session, userName: str):
-    return db.query(models.User).filter(models.User.userName == userName).first()
+
+def get_user_by_name(db: Session, user_name: str):
+    return db.query(models.User).filter(models.User.user_name == user_name).first()
+
+
+def db_create_post(user_name: str,
+                   post_title: str,
+                   description: str,
+                   is_nsfw: bool):
+    date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    db_post = models.Posts(
+        userName=user_name,
+        postTitle=post_title,
+        description=description,
+        date=date,
+        nsfw=is_nsfw
+    )
