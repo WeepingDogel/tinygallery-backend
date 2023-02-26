6b4f1d6462d6a97330fddd27c7aad55072ef740e
WeepingDogel <weepingdogel@gmail.com>
2023-02-26 19:41:05 +0800

    Database ORM Models.

--- app/model/models.py
+++ app/model/models.py
@@ -2,37 +2,36 @@ from sqlalchemy import Boolean, Column, Integer, String
 from ..db import Base
 
 
-
 class User(Base):
     __tablename__ = 'USERS'
     id = Column(Integer, primary_key=True, index=True)
-    userName = Column(String, nullable=False,
-                      sqlite_on_conflict_not_null='FAIL', unique=True)
+    user_name = Column(String, nullable=False,
+                       sqlite_on_conflict_not_null='FAIL', unique=True)
     password = Column(String, nullable=False,
                       sqlite_on_conflict_not_null='FAIL')
     date = Column(String, nullable=False, sqlite_on_conflict_not_null='FAIL')
     email = Column(String, nullable=False, sqlite_on_conflict_not_null='FAIL')
-    usersUUID = Column(String, nullable=False,
-                       sqlite_on_conflict_not_null='FAIL', unique=True)
+    users_uuid = Column(String, nullable=False,
+                        sqlite_on_conflict_not_null='FAIL', unique=True)
 
 
 class Posts(Base):
     __tablename__ = "POSTS"
     id = Column(Integer, primary_key=True, index=True)
-    postFilePath = Column(String, nullable=False,
-                          sqlite_on_conflict_not_null='FAIL')
-    postType = Column(String, nullable=False,
+    post_file_path = Column(String, nullable=False,
+                            sqlite_on_conflict_not_null='FAIL')
+    post_type = Column(String, nullable=False,
                        sqlite_on_conflict_not_null='FAIL')
-    coverFileType = Column(String, nullable=False,
-                           sqlite_on_conflict_not_null='FAIL')
-    postTitle = Column(String, nullable=False,
-                       sqlite_on_conflict_not_null='FAIL')
+    cover_file_type = Column(String, nullable=False,
+                             sqlite_on_conflict_not_null='FAIL')
+    post_title = Column(String, nullable=False,
+                        sqlite_on_conflict_not_null='FAIL')
     description = Column(String)
     dots = Column(Integer)
-    shareNum = Column(Integer)
+    share_num = Column(Integer)
     nfsw = Column(Boolean)
-    userName = Column(String, nullable=False,
-                      sqlite_on_conflict_not_null='FAIL')
+    user_name = Column(String, nullable=False,
+                       sqlite_on_conflict_not_null='FAIL')
     date = Column(String, nullable=False, sqlite_on_conflict_not_null='FAIL')
-    postUUID = Column(String, nullable=False,
-                      sqlite_on_conflict_not_null='FAIL')
+    post_uuid = Column(String, nullable=False,
+                       sqlite_on_conflict_not_null='FAIL')
