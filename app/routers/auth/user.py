051c7643f1adde0ad6b065d8308bbfbc9051e291
WeepingDogel <weepingdogel@gmail.com>
2023-02-27 17:13:04 +0800

    Routers and user.

--- app/routers/auth/user.py
+++ app/routers/auth/user.py
@@ -4,11 +4,13 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
+
 from ...model import schemas
 from ...model import crud
 from ...db import get_db
 from ... import config
+from ...dependencies.oauth2scheme import oauth2Scheme
 
 userAuthRouter = APIRouter(
     prefix="/auth",
@@ -54,35 +56,38 @@ def authenticate_user(db: Session, userName: str, password: str):
 
 
 @userAuthRouter.post("/register")
-async def create_user(user: schemas.User, db: Session = Depends(get_db)):
+def create_user(user: schemas.User, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_name(db, user_name=user.user_name)
+
     if user.user_name == "" or user.password == "" or user.email == "":
         raise HTTPException(
             status_code=400, detail="Username, password and email can't be empty.")
     if db_user:
         raise HTTPException(
             status_code=400, detail="Username has already existed!")
+
     user.password = get_password_hash(user.password)
     crud.create_user(db=db, user=user)
     return {"status": "success"}
-
-oauth2Scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 
 @userAuthRouter.post("/token")
 async def user_login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
     user_authentication = authenticate_user(
         db, form_data.username, form_data.password)
+
     if not user_authentication:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password.",
             headers={"WWW-Authenticate": "Bearer"}
         )
+
     access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": form_data.username},
         expires_delta=access_token_expires)
+
     return {"access_token": access_token, "token_type": "bearer"}
 
 
