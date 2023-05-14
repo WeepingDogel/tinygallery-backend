0026c0d5764bf33ce9b83094573cb974e0f19d21
WeepingDogel <weepingdogel@gmail.com>
2023-05-14 18:23:04 +0800

    Add comments

--- app/routers/auth/user.py
+++ app/routers/auth/user.py
@@ -1,3 +1,4 @@
+# Importing necessary libraries
 from fastapi import APIRouter, Depends, HTTPException
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
@@ -5,6 +6,8 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm
+
+# Importing modules from application
 from ...utilities.image_tools import generate_default_avatar
 from ...utilities.userdata_tool import get_user_uuid_by_name
 from ...model import schemas
@@ -14,6 +17,7 @@ from ... import config
 from ...dependencies.oauth2scheme import oauth2Scheme
 from ...utilities.token_tools import get_user_name_by_token
 
+# Defining router
 userAuthRouter = APIRouter(
     prefix="/user",
     tags=['User'],
@@ -24,17 +28,22 @@ userAuthRouter = APIRouter(
         }
     }
 )
+
+# Initializing password context for password encryption/decryption
 pwdContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
+# Function to verify password hash
 def verify_password(plain_password, hashed_password):
     return pwdContext.verify(plain_password, hashed_password)
 
 
+# Function to get password hash
 def get_password_hash(password):
     return pwdContext.hash(password)
 
 
+# Function to create access token
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
@@ -49,6 +58,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
+# Function to authenticate user
 def authenticate_user(db: Session, userName: str, password: str):
     user = crud.get_user_by_name(db, user_name=userName)
     if not user:
@@ -58,37 +68,46 @@ def authenticate_user(db: Session, userName: str, password: str):
     return True
 
 
+# Route for user registration
 @userAuthRouter.post("/register")
-# User Registration
 def create_user(user: schemas.User, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_name(db, user_name=user.user_name)
-    # Information check
+
+    # Check if username, password and email are empty or not
     if user.user_name == "" or user.password == "" or user.email == "":
         raise HTTPException(
             status_code=400, detail="Username, password and email can't be empty.")
+
+    # Check if the username already exists or not
     if db_user:
         raise HTTPException(
             status_code=400, detail="Username has already existed!")
-    # Password encrypt
+
+    # Encrypt the password and create user
     user.password = get_password_hash(user.password)
     crud.create_user(db=db, user=user)
-    # Give out the default avatar.
+
+    # Generate the default avatar for the user and return a success message.
     generate_default_avatar(user_uuid=get_user_uuid_by_name(user_name=user.user_name, db=db))
     return {"status": "success"}
 
 
+# Route for user login
 @userAuthRouter.post("/token")
 async def user_login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
     user_authentication = authenticate_user(
         db, form_data.username, form_data.password)
 
+    # Raise error if authentication fails
     if not user_authentication:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password.",
             headers={"WWW-Authenticate": "Bearer"}
         )
+
     try:
+        # Create access token
         access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
         access_token = create_access_token(
             data={"sub": form_data.username},
@@ -99,9 +118,12 @@ async def user_login(db: Session = Depends(get_db), form_data: OAuth2PasswordReq
             detail="Cannot create token.",
             headers={"WWW-Authenticate": "Bearer"}
         )
+
+    # Return access token
     return {"access_token": access_token, "token_type": "bearer"}
 
 
+# Route to update user data by username
 @userAuthRouter.put("/update/userdata/{user_name_for_update_data}")
 def update_user_data_by_user_name(user_name_for_update_data: str,
                                   db: Session = Depends(get_db),
@@ -110,6 +132,7 @@ def update_user_data_by_user_name(user_name_for_update_data: str,
     pass
 
 
+# Route to delete user by username
 @userAuthRouter.delete("/delete/{user_name_for_delete_user}")
 def delete_user_by_user_name(user_name_for_delete_user: str):
     pass
